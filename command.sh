cfc3111d19c3c22f3c7da8de50dd460fbfb6ea6c
karina <karinavu.january@gmail.com>
2024-04-30 18:01:25 +0700

    changed repo name

--- command.sh
+++ command.sh
@@ -4,7 +4,7 @@ case "$1" in
     "test")
         # run the tests
         echo "TestMachine is running the tests...."
-        cd /home/ubuntu/repo/ChatApp_Web_Testing
+        cd /home/ubuntu/repo/chat_app_web_testing_playwright 
         npm run test
         ;;
     "connect")
@@ -14,13 +14,13 @@ case "$1" in
     "serve")
         # run Express
         echo "Running Express server..."
-        cd /home/ubuntu/repo/ChatApp_Web_Testing
+        cd /home/ubuntu/repo/chat_app_web_testing_playwright 
         npm start > /dev/null 2>&1 &
         ;;
     "setup")
         # install dependencies and Playwright browser
         echo "Setting up dependencies..."
-        cd /home/ubuntu/repo/ChatApp_Web_Testing
+        cd /home/ubuntu/repo/chat_app_web_testing_playwright 
         npm install --dev
         npx playwright install --with-deps
         echo "Dependencies installed"
